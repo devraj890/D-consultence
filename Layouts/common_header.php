90071c9009d092d64be9c7f83ad3c02af39a14b5
Farukh Khan <112760002+farukh166@users.noreply.github.com>
2022-10-12 19:37:10 +0530

    updated

--- Layouts/common_header.php
+++ Layouts/common_header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>D Consultingin</title>
+    <title>D Consulting</title>
 
     <link rel="stylesheet" href="css/all.min.css">
     <!--link for font awesome 6.1..-->
